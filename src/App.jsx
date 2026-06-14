a8079dbf29f4d15e5e02be332b62ccbaf6f21636
Bilal Malik <bilalmlkdev@gmail.com>
2026-06-14 19:18:04 +0500

    style: upgrade html entrypoint shell with production meta tags and web app utilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import CSSGridGenerator from './components/CSSGridGenerator';
 import GithubIcon from './components/GithubIcon';
 import Header from './components/Header';
+import LogoIcon from './components/LogoIcon';
 
 export default function App() {
   return (
-    <div className="flex flex-col gap-10 w-full min-h-screen relative scrollbar-none overflow-hidden">
+    <div className="flex flex-col gap-12 w-full min-h-screen relative scrollbar-none overflow-hidden">
       <div className="absolute top-0 right-0 z-10">
         <GithubIcon />
+      </div>
+      <div className="absolute top-4 gap-1.5 left-5 z-10 flex items-center">
+        <LogoIcon height={7} width={7} />
+        <span className="text-white text-[22px] relative bottom-[1px]">Gridzzly</span>{' '}
       </div>
 
       <Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,16 @@
-import Logo from './Logo';
-
-// components/Header.jsx
 export default function Header() {
   return (
-    <header className="flex flex-col items-center text-center pt-14">
-      <div className="flex items-center gap-2 font-secondary">
-        <Logo />
-        <h1 className="text-5xl font-bold italic">
+    <header className="flex flex-col items-center text-center pt-12">
+      <div className="flex items-center gap-2 ">
+        <h1 className="text-5xl font-bold">
           CSS{' '}
-          <span className=" bg-gradient-to-r from-purple-400 to-purple-600 bg-clip-text text-transparent">
+          <span className="bg-gradient-to-r from-purple-400 to-purple-600 bg-clip-text text-transparent">
             Grid
           </span>{' '}
           Generator
         </h1>
       </div>
-      <p className="text-white/80 text-[20px] leading-[1] font-medium max-w-md font-script">
+      <p className="text-white/80 text-[20px] leading-[1] mt-1 font-medium max-w-md font-script">
         Design responsive CSS Grid layouts visually - draw items, customize tracks, and copy
         ready-to-use code.
       </p>
